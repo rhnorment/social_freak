df9f12ca4b048d841261ae2a791a304591925141
Hunt Norment <hunt@sherpa.red>
2016-06-24 15:50:37 -0500

    test twitter yaml

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+
+	def create
+		render text: request.env['omniauth.auth'].to_yaml
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   
   root to: 'pages#index', via: :get
 
+  get '/auth/:provider/callback', to: 'sessions#create'
+
 end
