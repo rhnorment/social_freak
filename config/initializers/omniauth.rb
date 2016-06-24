ba2d64ce3469038f476bfbbc6f11cbada0050504
Hunt Norment <hunt@sherpa.red>
2016-06-24 16:01:31 -0500

    test twitter yaml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-	provider :twitter, ENV['EFKtSg961sZxYfyS0NxUFEewC'], ENV['KnDYCuGs1HhBxZmvCreJ1nPRGpViI9AulTIAcVz4AWLtMG45HC']	
+	provider :twitter, ENV['TWIITER_KEY'], ENV['TWITTER_SECRET']	
 end
