8c207ff7a36291a34168afc70f305da848246797
Hunt Norment <hunt@sherpa.red>
2016-06-28 17:05:06 -0500

    add linkedin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 		user.name = auth_hash['info']['name']
 		user.location = get_social_location_for(user.provider, auth_hash['info']['location'])
 		user.image_url = auth_hash['info']['image']
-		user.url = get_social_location_for(user.provider, auth_hash['info']['urls'])
+		user.url = get_social_url_for(user.provider, auth_hash['info']['urls'])
 		user.save!
 		user
 	end
